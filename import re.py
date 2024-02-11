ed74aa05c7858d021d5379190a2819276bd6bec3
Camila <c.fajardos@uniandes.edu.co>
2024-02-11 18:07:18 -0500

    variable y asignacion

--- import re.py
+++ import re.py
@@ -62,22 +62,6 @@ def lexer(datos):
                     
     return tokens
 
-def analizeDefVariable():
-    pass
-
-def analizeDirection():
-    pass
-
-def analizeOrientation():
-    pass
-
-def analizeConstant():
-    pass
-
-
-
-
-
 def espaciosParentesis(linea):
     if linea.__contains__("("):
         linea = linea.replace("(", " ( ")
@@ -87,6 +71,96 @@ def espaciosParentesis(linea):
 
     
     return linea
+
+listaVariables = []
+
+def programa(tokens):
+    while tokens:
+        comando(tokens)
+
+def comando(tokens):
+    if tokens[1][0] == 'DEFINITION_VARIABLE':
+        analizeDefVariable(tokens)
+    elif tokens[1][0] == 'ASSIGNMENT':
+        analizeAsignacion(tokens)
+    elif tokens[1][0] == 'COMMAND':
+        analizeCommand(tokens)
+    elif tokens[1][0] == 'CONTROL_STRUCTURE':
+        control_estructura(tokens)
+    elif tokens[1][0] == 'ELEMENT':
+        funcion_llamada(tokens)
+    elif tokens[0][0] and tokens[0][1] == "LPAREN":
+        pass
+
+
+def analizeDefVariable(tokens):
+    if tokens[0][0] == "LPAREN":
+        tokens.pop(0)
+        if tokens[0][0] == 'DEFINITION_VARIABLE':
+            tokens.pop(0)  # Consumir el token DEFINITION_VARIABLE
+            if tokens[0][0] == 'ELEMENT':
+                tokens.pop(0)  # Consumir el token ELEMENT
+                if tokens[0][0] == 'NUMBER'  or "CONSTANT":
+                    tokens.pop(0)
+                    listaVariables.append(tokens[0])
+                    if tokens[0][0] == "ELEMENT" and tokens[0][1] in listaVariables:
+                        tokens.pop(0)  # Consumir el token NUMBER
+                        if tokens[0][0] == "RPAREN":
+                            tokens.pop(0)
+                        else:
+                            print("False")
+                    else:
+                        print("False")
+                else:
+                    print("False")
+            else:
+                print("False")
+        else:
+            print("False")
+    else:
+        print("False")
+
+def analizeAsignacion(tokens):
+    if tokens[0][0] == "LPAREN":
+        tokens.pop(0)
+        if tokens[0][0] == 'ASSIGMENT':
+            tokens.pop(0)  # Consumir el token DEFINITION_VARIABLE
+            if tokens[0][0] == 'ELEMENT':
+                tokens.pop(0)  # Consumir el token ELEMENT
+                if tokens[0][0] == 'NUMBER'  or "CONSTANT":
+                    tokens.pop(0)
+                    listaVariables.append(tokens[0])
+                    if tokens[0][0] == "ELEMENT" and tokens[0][1] in listaVariables:
+                        tokens.pop(0)  # Consumir el token NUMBER
+                        if tokens[0][0] == "RPAREN":
+                            tokens.pop(0)
+                        else:
+                            print("False")
+                    else:
+                        print("False")
+                else:
+                    print("False")
+            else:
+                print("False")
+        else:
+            print("False")
+    else:
+        print("False")
+
+        
+
+
+def analizeCommand():
+    pass
+
+def analizeConstant():
+    pass
+
+
+
+
+
+
     
 
 
